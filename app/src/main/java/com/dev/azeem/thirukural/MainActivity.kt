a31c6deb9cd662afc72b5c1a6441febdc189c57f
Azeem <mohammed.azeem@influx.co.in>
2024-06-16 14:21:41 +0530

    First commit

--- app/src/main/java/com/dev/azeem/thirukural/MainActivity.kt
+++ app/src/main/java/com/dev/azeem/thirukural/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dev.azeem.thirukural.ui.theme.ThirukuralTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,43 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ThirukuralTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+    Scaffold(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.purple_200))
+    ) { innerPadding ->
+        Greeting(
+            name = "Thirukural",
+            modifier = Modifier
+                .padding(innerPadding)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box(modifier = Modifier
+        .fillMaxSize(),
+        contentAlignment = Alignment.Center) {
+        Text(
+            text = "Welcome to $name!",
+            modifier = modifier.padding(6.dp)
+        )
+    }
+}
+
+@Preview(showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     ThirukuralTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
